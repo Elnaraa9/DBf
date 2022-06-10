2c6151d116922b88bee93a95371dfc353ad0064d
Elnaraa9 <7khtht32code.edu.az>
2022-06-10 08:30:21 +0400

    axsamdan commit etmeyi unutmusam

--- Fiorelloo/WebApplication1/Areas/AdminPanel/Controllers/SliderController.cs
+++ Fiorelloo/WebApplication1/Areas/AdminPanel/Controllers/SliderController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.DAL;
+using WebApplication1.Models;
 
 namespace WebApplication1.Areas.AdminPanel.Controllers
 {
@@ -18,6 +19,14 @@ namespace WebApplication1.Areas.AdminPanel.Controllers
         public IActionResult Index()
         {
             return View(_db.Slides);
+        }
+        public IActionResult Create(Slide slide)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return View();
         }
     }
 }
